392d7957bcdfcef172c637c0736cae2691ffb361
nabm <nathabmmb@gmail.com>
2023-05-29 13:11:09 -0300

    Agregar archivos de proyecto.

---
+++ IVR/OpcionValidacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IVR
+{
+    internal class OpcionValidacion
+    {
+        string descripcion;
+        bool correcta;
+
+        OpcionValidacion (string descripcion, bool correcta)
+        {
+            this.descripcion = descripcion;
+            this.correcta = correcta;
+        }
+
+        internal bool EsCorrecta() { return correcta; }
+    }
+}
---
+++ IVR/Validacion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IVR
+{
+    internal class Validacion
+    {
+        string nombre;
+        int numeroOrden;
+        string audioMensajeValidacoin;
+        string mensajeValidacion;
+
+        Validacion (string nombre, int numeroOrden, string mensajeValidacion, string audioMensajeValidacoin)
+        {
+            this.nombre = nombre;
+            this.numeroOrden = numeroOrden;
+            this.mensajeValidacion = mensajeValidacion;
+            this.audioMensajeValidacoin = audioMensajeValidacoin;
+        }
+    }
+}
